54072936a1331c6caabf96586e973cc836913a7d
Surojit Ain <surojitain@gmail.com>
2015-10-15 11:19:47 +0530

    HybridTestAutomationFramework (HTAF), release 1.0

---
+++ HybridTestAutomationFramework/src/com/htaf/libraryfunctions/Constants.java
@@ -0,0 +1,15 @@
+package com.htaf.libraryfunctions;
+
+public class Constants {
+	
+    public static String KEYWORD="Keyword";
+    public static String KEYWORD_PASS="PASS";
+    public static String KEYWORD_FAIL="FAIL";
+    public static String RESULT="Result";
+    public static String KEYWORD_SKIP="SKIP";
+    public static String CONFIG="config";
+    public static final String SUITE_ID = "TSID";
+    
+    public static final String PATH_SCREENSHOT = System.getProperty("user.dir") + "\\src\\Screenshots\\";
+
+}
